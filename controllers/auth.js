95511bdd7b082fa1d58208e1ae18780f56acfeeb
FurkanPortakal <furkanportakalx@gmail.com>
2021-02-08 19:08:31 +0300

    :bug: fixed
    fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,10 +11,7 @@ exports.register = async (req, res, next) => {
       password
     })
 
-    res.status(201).json({
-      success: true,
-      user
-    })
+    sendToken(user, 201, res)
   } catch (error) {
     next(error)
   }
@@ -39,10 +36,7 @@ exports.login = async (req, res, next) => {
       res.status(404).json({ success: false, error: 'Invalid credentials' })
     }
 
-    res.status(200).json({
-      success: true,
-      token: 'tr34f3443fc'
-    })
+    sendToken(user, 200, res)
   } catch (error) {
     res.status(500).json({ success: false, error: error.message })
   }
@@ -54,4 +48,9 @@ exports.forgotpassword = (req, res, next) => {
 
 exports.resetpassword = (req, res, next) => {
   res.send('Reset Password Route')
+}
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getSignedToken()
+  res.status(statusCode).json({ success: true, token })
 }
